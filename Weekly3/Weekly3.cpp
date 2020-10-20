bfdae339a32ddb68ac04973a6051d66af6990f84
Eddy1108 <69895660+Eddy1108@users.noreply.github.com>
2020-10-20 10:57:37 +0200

    Moved everything to a header file

--- Weekly3/Weekly3.cpp
+++ Weekly3/Weekly3.cpp
@@ -3,104 +3,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-
-int randomNumber{ 0 };
-int difficulty{ 5 };        //The highest number for each difficulty is stored here. (defaults to easy)
-int guess{ 0 };
-int highscore{ 0 };         //Where the highscore for each round is stored
-int highscore1{ 3 };       //Highscore for Easy
-int highscore2{ 5 };       //Highscore for Medium
-int highscore3{ 8 };       //Highscore for Hard
-int attempts{ 0 };
-
-bool i = true;      //Used to keep the while loop to continue while choosing a difficulty, set to false when a valid selection is made.
-
-void setDifficulty()
-{
-    int difChoice{ 1 };
-
-    while (i == true)
-    {
-        std::cout << "Please choose a difficuly: \n[1] Easy \n[2] Medium \n[3] Hard \n";
-        std::cin >> difChoice;
-
-        switch (difChoice)
-        {
-        case 1:
-            difficulty = 5;
-            highscore = highscore1;
-            i = false;
-            break;
-        case 2:
-            difficulty = 10;
-            highscore = highscore2;
-            i = false;
-            break;
-        case 3:
-            difficulty = 30;
-            highscore = highscore3;
-            i = false;
-            break;
-        default:
-            std::cout << "That is not a valid option!\n";
-            system("pause");
-            system("cls");
-            break;
-        }
-    }
-}
-
-void generateNumber() 
-{
-    std::srand(static_cast<unsigned int>(time(nullptr)));       //changes rand() functions seed based on the system clock, if not i get the same "random" numbers each time
-
-    for (int i = 0; i < 5; i++) {
-        randomNumber = rand() % difficulty + 1;
-    }
-}
-
-void play() 
-{
-    system("cls");
-    std::cout << "Guess what the random number is: (1-" << difficulty << ").\n\n";
-    std::cin >> guess;
-
-    while (true)    //This is where the game happens.
-    {
-        attempts++;
-        if (guess == randomNumber)
-        {
-            break;
-        }
-        else if (guess > randomNumber) {
-            std::cout << "Guess Lower!\n";
-        }
-        else if (guess < randomNumber) {
-            std::cout << "Guess Higher!\n";
-        }
-        std::cin >> guess;
-    }
-
-    //Post-game 
-    std::cout << "\nYou guessed correctly in " << attempts << " tries!";
-    if (attempts < highscore) {
-        std::cout << "\nYou beat the highscore!" << "\nThe Highscore was " << highscore;
-        switch (difficulty) {
-        case 5:
-            highscore1 = attempts;      //Storeing the new highscore for each difficulty respectively.
-            break;
-        case 10:
-            highscore2 = attempts;
-            break;
-        case 30:
-            highscore3 = attempts;
-            break;
-        }
-    }
-    else {
-        std::cout << "\nThe Highscore is: " << highscore;
-    }
-}
+#include "VariableHeader.h"
 
 int main()
 {
@@ -117,4 +20,3 @@ int main()
         system("cls");
     }
 }
-//Is 120 lines of code perhaps a bit much for a simple number guessing game??
